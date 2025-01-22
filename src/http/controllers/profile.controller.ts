9bc2ceb323756f0b51422a52d7119dd55c0f32a8
eduardobertozi <webdu15@gmail.com>
2025-01-22 13:53:16 -0300

    :sparkles: feat: add user profile retrieval logic with JWT verification

--- src/http/controllers/profile.controller.ts
+++ src/http/controllers/profile.controller.ts
@@ -1,5 +1,20 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  return reply.code(200).send()
+  await request.jwtVerify()
+  console.log(request.user.sub)
+
+  const getUserProfile = makeGetUserProfileUseCase()
+
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.code(200).send({
+    user: {
+      ...user,
+      password_hash: null,
+    },
+  })
 }
