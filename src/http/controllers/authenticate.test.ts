b97e12c2380faf2038f8e1d438e71bf975a3b2b3
eduardobertozi <webdu15@gmail.com>
2025-02-03 09:22:58 -0300

    test: add end-to-end tests for user authentication and profile retrieval

--- src/http/controllers/authenticate.test.ts
+++ src/http/controllers/authenticate.test.ts
@@ -1,3 +1,30 @@
-test('ok', () => {
-  expect(1).toBe(1)
+import request from 'supertest'
+import { app } from '@/app'
+
+describe('Authenticate (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
 })
