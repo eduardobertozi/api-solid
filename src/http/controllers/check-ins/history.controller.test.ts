0c7e95c2ab5637383e3793b3ab15c0d020be67f7
eduardobertozi <webdu15@gmail.com>
2025-02-03 13:00:27 -0300

    :sparkles: feat: add end-to-end tests for check-in creation, history, metrics, and validation

---
+++ src/http/controllers/check-ins/history.controller.test.ts
@@ -0,0 +1,61 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Check-in History (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to list of check-ins', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const user = await prisma.user.findFirstOrThrow()
+
+    // Usando diretamente o prisma
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Javascript gym',
+        description: 'Some description.',
+        phone: '1199999999',
+        latitude: -26.7201952,
+        longitude: -48.6899712,
+      },
+    })
+
+    await prisma.checkIn.createMany({
+      data: [
+        {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+        {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+      ],
+    })
+
+    const response = await request(app.server)
+      .get('/check-ins/history')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.checkIns).toEqual([
+      expect.objectContaining({
+        gym_id: gym.id,
+        user_id: user.id,
+      }),
+      expect.objectContaining({
+        gym_id: gym.id,
+        user_id: user.id,
+      }),
+    ])
+  })
+})
